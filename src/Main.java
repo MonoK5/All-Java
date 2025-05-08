f5960c99760bee4d2c7dddb814b60be1c4e06ae0
khosatho <monokhosatho@gmail.com>
2025-05-08 09:45:29 +0200

    added for each loop

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-        Scanner input = new Scanner(System.in);
-
-//        System.out.println("what is your name? \n How old are you? \n How much do you earn?");
-        System.out.print("Calculate");
-
-        int time = input.nextInt();
-        int price = input.nextInt();
-        int tip = input.nextInt();
-        int total = price * time + tip ;
-
-
-        System.out.println(time * price + tip);
-    }
-}
+//import java.util.Scanner;
+//
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Scanner input = new Scanner(System.in);
+//
+////        System.out.println("what is your name? \n How old are you? \n How much do you earn?");
+//        System.out.print("Calculate");
+//
+//        int time = input.nextInt();
+//        int price = input.nextInt();
+//        int tip = input.nextInt();
+//        int total = price * time + tip ;
+//
+//
+//        System.out.println(time * price + tip);
+//    }
+//}
 
 //
 //        String name = input.nextLine();
@@ -53,3 +53,37 @@ public class Main {
 ////
 ////
 ////
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("search for the name");
+
+
+        String[] names = {"Gugu", "Lindelani", "Mbali", "Bob"};
+        String target = scanner.nextLine();
+        boolean found = false;
+        int index = scanner.nextInt();
+
+        System.out.println("Enter your name");
+        for (String name : names) {
+            if (name.contains(target)) {
+                System.out.println("You searched for " + name);
+
+                found = true;
+                break;
+
+            }
+
+
+        }
+        if (!found) {
+            System.out.println("That name does not exist");
+        }
+    }
+}
